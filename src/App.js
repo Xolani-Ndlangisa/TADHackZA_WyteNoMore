ba7f2f1c87c5604662d800e6119c073173f7dcaa
sntjobok <sntjobok@student.wethinkcode.co.za>
2021-09-25 00:20:01 +0200

    just a start up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          {/* Edit <code>.js</code> and save to reload. */}
-          Wait-NO-MO
-        </p>
-        <a
-          // className="App-link"
-          // href="https://reactjs.org"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-          {/* Learn React */}
-          about us
-        </a>
-      </header>
+    <div className='App'>
+      <h1>HEllo</h1>
     </div>
+
   );
 }
 
