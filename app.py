c813c2f924f9a31255dcc5f27d421d1b2366f6c4
xndlangi <xndlangi@student.wethinkcode.co.za>
2021-09-25 12:47:15 +0200

    configs

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, json, request, jsonify
 import auth.auth as auth
 app = Flask(__name__)
 
 user = auth.Auth()
 @app.route('/')
 def hello_world():
-    return "<h1>Hello World!</h1>"
+    return json.dumps({"result": "request API Documantation"})
 
 @app.route('/api/otp')
 def opt():
@@ -27,5 +27,6 @@ def signup():
         return status
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
 
