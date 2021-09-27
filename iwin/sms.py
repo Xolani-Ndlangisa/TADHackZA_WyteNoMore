8a933edcdc35fb2c157705701cdd8a0cde6d840d
Mahlangu <34237978+PTMahlangu@users.noreply.github.com>
2021-09-27 07:54:08 +0200

    Update sms.py

--- iwin/sms.py
+++ iwin/sms.py
@@ -11,7 +11,7 @@ def sendSMS(data,msg):
     response = requests.post(
         'https://api.iwin.co.za/iwin/api/v1/messages',
         data =data,
-        headers= {'Authorization' : 'bearer 679fc77f7336456aa1b5bccb30b2a6ff' }
+        headers= {'Authorization' : 'bearer {token}' }
     )
     return response
 
